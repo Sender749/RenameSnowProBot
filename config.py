2c4486a4dc54c7369d0573b7eb38d7dba0e605de
Sender749 <jotmahal2003@gmail.com>
2025-01-11 18:34:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_HASH = os.environ.get("API_HASH", "")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "0") 
 
 DB_NAME = os.environ.get("DB_NAME","User_Data")     
 
